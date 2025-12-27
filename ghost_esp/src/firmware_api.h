118693fad96c03503613f559fcfbeb5e41a18734
grugnoymeme <47lecoste@tuta.io>
2025-12-27 20:58:21 +0100

    update apps

--- ghost_esp/src/firmware_api.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <furi.h>
-#include <furi_hal_version.h>
-
-// Check if we're building for Momentum firmware
-#if defined(FW_ORIGIN_NoName)
-#include <noname/noname.h>
-#define HAS_NONAME_SUPPORT 1
-#endif
